ff3c12a0cf94885aa7dddad1db5edb5c46cd9bf1
Sergio Enrech Trillo <senrech@yahoo.fr>
2016-06-01 19:51:47 +0100

    add challenge 11 - testing behaviour

--- app/js/controllers/ToDoController.js
+++ app/js/controllers/ToDoController.js
@@ -1,3 +1,14 @@
-toDoApp.controller('ToDoController', [function() {
-  this.todo = "ToDo1";
-}]);
+toDoApp.controller('ToDoController', function() {
+  //2 objects, each with 2 attributes
+  self = this;
+  self.todos = [{text: "ToDo1", completed: true}, {text: "ToDo2", completed: false }];
+
+  self.addToDo = function(todoText) {
+    self.todos.push({text: todoText, completed: false});
+  };
+
+  self.removeToDo = function() {
+    self.todos.pop();
+  };
+
+});
--- test/unit/ToDoController.spec.js
+++ test/unit/ToDoController.spec.js
@@ -7,7 +7,33 @@ describe('ToDoController', function() {
     ctrl = $controller('ToDoController');
   }));
 
-  it('initialises with a toDo', function() {
-    expect(ctrl.todo).toEqual("ToDo1");
+  it('initialises with severals todos', function() {
+    var todos = [
+      {text: "ToDo1", completed: true},
+      {text: "ToDo2", completed: false}
+    ];
+    expect(ctrl.todos).toEqual(todos);
+  });
+
+  it('adds a task', function() {
+    ctrl.addToDo("NewToDo");
+    var newtask = { text: "NewToDo", completed: false };
+    expect(ctrl.todos).toContain(newtask);
+  });
+
+  it('removes a task', function() {
+    var todo1 = {text: "ToDo1", completed: true};
+    var todo2 = {text: "ToDo2", completed: false};
+    ctrl.addToDo("NewToDo");
+    ctrl.removeToDo();
+    expect(ctrl.todos).toContain(todo1);
+    expect(ctrl.todos).toContain(todo2);
+    expect(ctrl.todos).not.toContain("NewToDo");
+  });
+
+  it('removes a task - different approach', function(){
+    var initialCount = ctrl.todos.length;
+    ctrl.removeToDo();
+    expect(ctrl.todos.length).toEqual(initialCount-1);
   });
 });
